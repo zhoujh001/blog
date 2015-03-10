7764f6013055f90a040947ca4cf0e71c189ffb32
zjh599245299 <599245299@qq.com>
2015-03-10 10:30:00 +0800

    2015-3-10 10:29

--- src/common/BaseRoute.java
+++ src/common/BaseRoute.java
@@ -16,17 +16,28 @@ public class BaseRoute extends Routes {
     @Override
     public void config() {
         AutoBindRoutes autoBindRoutes = new AutoBindRoutes();
+        //获取自动扫描的路径
         String scannerDir = getProperties("config.properties");
+        //获取扫描包后的类名
         String[] ctrlName = autoBindRoutes.scanAllCtrl(scannerDir);
         for (String name : ctrlName) {
+            //解析自动注册的Controller，并注册访问路径
             String[] names = name.split("\\.");
             String addressName = names[names.length-2];
+            //访问路径为类名去除后面的Controller为访问改Controller的根路径
+            //例如：AdminController类的访问路径为/admin
             addressName = addressName.substring(0,addressName.length()-10).toLowerCase();
+
             add("/" + addressName, getClassByName(name), "/");
         }
 
     }
 
+    /**
+     * 读取配置文件中自动注册路由的扫描路径
+     * @param s
+     * @return
+     */
     private String getProperties(String s) {
         Properties properties = new Properties();
         try {
@@ -39,6 +50,11 @@ public class BaseRoute extends Routes {
         return null;
     }
 
+    /**
+     * 返回扫描包下的Controller的Class对象
+     * @param name
+     * @return
+     */
     private Class getClassByName(String name) {
         name = name.substring(0,name.indexOf(".class"));    //去除类的全路径后的.class
         Class clazz;
@@ -49,9 +65,5 @@ public class BaseRoute extends Routes {
             e.printStackTrace();
         }
         return null;
-    }
-
-    public static void main(String[] args) {
-        new BaseRoute().config();
     }
 }
--- src/utils/AutoBindRoutes.java
+++ src/utils/AutoBindRoutes.java
@@ -7,25 +7,33 @@ import java.net.URL;
  * Created by Administrator on 2015/3/9.
  * 自动扫描controller包中的所有路径，并注册到jfinal中
  */
-public class AutoBindRoutes{
+public class AutoBindRoutes {
     static URL ctrlPath = AutoBindRoutes.class.getResource("/");
 
+    /**
+     * 扫描指定文件夹下的类，分析出Controller.class和访问路径，并自动注册到路由中
+     *
+     * @param ctrlDirName
+     * @return
+     */
     public String[] scanAllCtrl(String ctrlDirName) {
+        //保存参数值
         String oldCtrlDirNmae = ctrlDirName;
+        //按路由的指定格式解析
         ctrlDirName = ctrlPath + "/" + ctrlDirName;  //controller文件夹的路径
         ctrlDirName = ctrlDirName.substring(6, ctrlDirName.length());
         File file = new File(ctrlDirName);
-        String[] ctrlClassName ;
+        String[] ctrlClassName;
         if (file.isDirectory()) {
             String[] list = file.list();
             ctrlClassName = new String[list.length];
             int i = 0;
             for (String fileName : list) {
-                ctrlClassName[i] = oldCtrlDirNmae+"."+fileName;
+                ctrlClassName[i] = oldCtrlDirNmae + "." + fileName;
                 i++;
             }
         } else {
-            throw(new RuntimeException("Controller Package is Error."));
+            throw (new RuntimeException("Controller Package is Error."));
         }
 
         return ctrlClassName;
